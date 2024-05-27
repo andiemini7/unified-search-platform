5fd3949e91e43834af4219de76ef03407fb19156
eerzagashiii <erza.gashi02@gmail.com>
2024-05-27 16:09:03 +0200

    composer installer

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'hp/unified-search-platform',
+        'pretty_version' => 'dev-develop',
+        'version' => 'dev-develop',
+        'reference' => '669d3369fb6f984b9062dcb1eb19c023a0ba81f3',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'hp/unified-search-platform' => array(
+            'pretty_version' => 'dev-develop',
+            'version' => 'dev-develop',
+            'reference' => '669d3369fb6f984b9062dcb1eb19c023a0ba81f3',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
