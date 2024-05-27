c51bfa53442154c7af2356529fc54664739c2ff3
egzonasylejmani <egzonasylejmanni@gmail.com>
2024-05-27 22:14:52 +0200

    Endpoint

--- functions.php
+++ functions.php
@@ -27,6 +27,40 @@ function register_my_menus() {
 add_action('wp_enqueue_scripts', 'my_theme_enqueue_styles');
 add_action('init', 'register_my_menus');
 
+
+// Endpoint
+function my_custom_endpoint() {
+    register_rest_route('myplugin/v1', '/pages', array(
+        'methods' => 'GET',
+        'callback' => 'custom_get_pages',
+    ));
+}
+add_action('rest_api_init', 'my_custom_endpoint');
+
+
+function custom_get_pages() {
+    $args = array(
+        'post_type' => 'page',
+        'posts_per_page' => 10, 
+    );
+    $query = new WP_Query($args);
+
+    $pages = array();
+    if ($query->have_posts()) {
+        while ($query->have_posts()) {
+            $query->the_post();
+            $pages[] = array(
+                'id' => get_the_ID(),
+                'title' => get_the_title(),
+                'description' => get_the_excerpt(),
+            );
+        }
+        wp_reset_postdata();
+    }
+
+    return new WP_REST_Response($pages, 200);
+}
+
 require get_template_directory() . '/enqueues.php';
 require get_template_directory() . '/init.php';
 
