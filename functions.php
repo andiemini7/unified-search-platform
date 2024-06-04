6bd97350077141d95e60671cfc5fd00ea41fd149
eerzagashiii <ereza.gashi02@gmail.com>
2024-06-04 14:02:33 +0200

    added the vendor, replaced the "require"statements with "use".

--- functions.php
+++ functions.php
@@ -1,3 +1,15 @@
 <?php
-
+require get_template_directory() . '/vendor/autoload.php';
 require get_template_directory() . '/includes/init.php';
+
+function add_tailwind_css() {
+    
+    wp_enqueue_style('tailwind-css', 'https://cdnjs.cloudflare.com/ajax/libs/tailwindcss/2.2.19/tailwind.min.css');
+}
+
+
+add_action('wp_enqueue_scripts', 'add_tailwind_css');
+
+?>
+
+
--- includes/init.php
+++ includes/init.php
@@ -1,27 +1,39 @@
 <?php
-require_once('enqueue.php');
-require_once('setup.php'); 
+
+namespace Hp\UnifiedSearchPlatform;
+
+use Hp\UnifiedSearchPlatform\Enqueue;
+use Hp\UnifiedSearchPlatform\Setup;
+use Hp\UnifiedSearchPlatform\App\postTypes\PostTypes;
 
 class Init {
     public function __construct() {
+        $setup = new Setup();
+        add_action('after_setup_theme', [$setup, 'unifiedsearch_setup']);
         add_action('init', [$this, 'register_documentations']);
         add_action('init', [$this, 'documentation_taxonomies']);
-        add_action('wp_enqueue_scripts', 'enqueueStyles'); 
-        add_action('after_setup_theme', 'unifiedsearch_setup');
-
-
+        $enqueue = new Enqueue();
+        add_action('wp_enqueue_scripts', [$enqueue, 'enqueueStyles']);
     }
 
     public function register_documentations() {
-        require_once get_template_directory() . '/app/PostTypes/PostTypes.php';
-        register_documentations(); 
+        $postTypes = new PostTypes();
+        $postTypes->register_documentations();
     }
 
     public function documentation_taxonomies() {
-        require_once get_template_directory() . '/app/PostTypes/PostTypes.php';
-        documentation_taxonomies();
+        $postTypes = new PostTypes();
+        $postTypes->documentation_taxonomies();
+    }
+
+    public function enqueueStyles() {
+        // Enqueue your styles here
+    }
+
+    public function unifiedsearch_setup() {
+        // Setup theme support, menus, etc.
     }
 }
 
+// Initialize the theme setup
 new Init();
-?>
