3493c75223f1f997f5a5e98440f41c99c652df8b
Altin Ramadani <125731622+alt1nn@users.noreply.github.com>
2024-06-05 21:16:37 +0200

    Update header.php

--- header.php
+++ header.php
@@ -6,7 +6,6 @@
     <?php wp_head(); ?>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
     <link rel="stylesheet" href="style.css">
-    <script src="js/drop-down.js"></script> 
 </head>
 <body <?php body_class('flex flex-col min-h-screen'); ?>>
 
