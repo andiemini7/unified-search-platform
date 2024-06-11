182ce1ee3d891a6ba0e6bee1ed89ce67d0429ad6
Altin Ramadani <altinramadani3@gmail.com>
2024-06-11 10:25:19 +0200

    Files moved

--- includes/enqueue.php
+++ includes/enqueue.php
@@ -9,6 +9,4 @@ function enqueueStyles() {
     wp_enqueue_style('theme-styles', get_stylesheet_uri(), ['tailwindcss'], wp_get_theme()->get('Version'));
     wp_enqueue_script('main-script', get_template_directory_uri() . '/assets/dist/js/main.js', ['jquery'], null, true);
 }
-
-
-
+}
