02991ea250c6ecf98e3c8c69c40fb2fbb7925ff9
Urim Rexhepi <urimm.r@gmail.com>
2024-06-12 11:59:43 +0200

    implementation of SSO Authentication

--- functions.php
+++ functions.php
@@ -2,7 +2,8 @@
 
 require get_template_directory() . '/vendor/autoload.php';
 require_once get_template_directory() . '/includes/employee-registration.php';
+require_once get_template_directory() . '/includes/employee-login.php';
 require get_template_directory() . '/includes/init.php';
-
 require get_template_directory() .'/app/acf/acf.php';
-
+require get_template_directory() . '/includes/widgets.php';
+require get_template_directory() . '/includes/redirect-login.php';
