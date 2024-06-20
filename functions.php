c4e5414c282d2200400e70e7c1d2d52e3a316795
Urim Rexhepi <urimm.r@gmail.com>
2024-06-20 10:40:50 +0200

    create custom post types Members and Teams

--- functions.php
+++ functions.php
@@ -4,6 +4,8 @@ require get_template_directory() . '/vendor/autoload.php';
 require_once get_template_directory() . '/includes/employee-registration.php';
 require_once get_template_directory() . '/includes/employee-login.php';
 require get_template_directory() . '/includes/init.php';
+require get_template_directory() . '/app/cpt/cpt-members.php';
+require get_template_directory() . '/app/cpt/cpt-teams.php';
 
 
 // API Endpoint
