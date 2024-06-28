0dadc8d19eedbcdb1f6c4ceb05e301ec68299652
Urim Rexhepi <urimm.r@gmail.com>
2024-06-28 10:40:59 +0200

    redirect bug fix

--- modules/Text_Block.php
+++ modules/Text_Block.php
@@ -7,9 +7,9 @@
 
 ?>
 
-<div class="w-full">
+<!-- <div class="w-full">
     <div class="p-8 bg-white text-center background-color:#E7F0F9;">
         <p class="text-lg"><?php echo esc_html($text); ?></p>
         <img src="<?php echo esc_url( $image['url'] ); ?>"  style="height:100px;margin:auto" alt="<?php echo esc_attr( $image['alt'] ); ?>" />
     </div>
-</div>
+</div> -->
